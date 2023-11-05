433e30dcedf3f029177bc9580ac4f3c1d2d0ab58
Oscar Herrera <105088864+Oscar-F-Herrera@users.noreply.github.com>
2023-11-04 20:27:14 -0600

    Actividad 12

--- src/edu/oscar/herrera/utils/math/Factorial.java
+++ src/edu/oscar/herrera/utils/math/Factorial.java
@@ -1,12 +1,37 @@
 package edu.oscar.herrera.utils.math;
 
+/**
+ * La clase Factorial proporciona métodos para calcular el factorial de un número de forma recursiva e iterativa
+ */
 public class Factorial {
-    public static int get(int n){
+    /**
+     * Calcula el factorial de un número de manera recursiva
+     *
+     * @param n El número entero del cual se desea calcular el factorial
+     * @return El factorial del número n
+     */
+    public static int getRecursive(int n){
         if (n == 1 || n == 0){
             return 1;
         }
         else {
-            return n * get(n-1);
+            return n * getRecursive(n-1);
         }
+    }
+
+    /**
+     * Calcula el factorial de un número de manera iterativa
+     *
+     * @param n El número entero del cual se desea calcular el factorial
+     * @return El factorial del número n
+     */
+    public static int getIterative(int n){
+        int sum = 1;
+
+        for (int i = n; i > 0 ; i--) {
+            sum *= i;
+        }
+
+        return sum;
     }
 }
